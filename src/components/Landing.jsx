772d88e47a4ec5edaa4a21ff4534535a09f40405
kimhengpeng <kpeng@sfsu.edu>
2023-06-27 20:28:51 +0700

    Finished Landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,41 +1,57 @@
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import Mario from '../assets/giphy.gif';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import Mario from '../assets/mario-coding.gif';
+import Duck from '../assets/duck.gif';
 
 const Landing = ({ setSelectedPage }) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [text] = useTypewriter({
+    words: ['a Software Engineer', 'a Full Stack Web Developer', 'Challenger'],
+    loop: {},
+  })
   return (
-    <section id='home' className="md:flex md:justify-center md:item-center md:h-full py-10">
+    <section id='home' className="md:flex md:justify-center md:item-center md:h-full py-10 bg-gradient-custom-1">
       {/* RIGHT SIDE */}
-      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+      <div className="md:order-2 flex justify-center basis-2/4 z-10 mt-16 md:mt-16">
         {isAboveMediumScreens ? (
           <div className="relative flex justify-center item-center overflow-hidden">
-            <img className="" src={Mario}></img>
+            <img className="rounded-full" src={Duck}></img>
           </div>
         ) : (
           <div></div>
         )}
       </div>
       {/* LEFT SIDE */}
-      <div className="z-30 basis-2/5 mt-12 md:mt-32 mx-auto">
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{once: true, amount: 0.5}}
-            transition={{duration: 1, ease: "easeOut"}}
-            variants={{
-              hidden: {opacity: 0, x: -50},
-              visible: {opacity: 1, x: 0}
-            }}
-          >
-            <p className="text-3xl font-playfair z-10 text-center md:text-start">
-              Hi, I'm {""}
-              <span className="xs:relative xs:text-dark-grey font-semibold z-20">
-                Kimheng
-              </span>
-            </p>
-          </motion.div>
+      <div className="flex justify-center item-center z-30 basis-2/4 mt-12 md:mt-32">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 }
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start md:mt-[100px]">
+            Hi, I'm {""}
+            <span className="xs:relative font-semibold text-yellow z-20">
+              Kimheng
+            </span>
+          </p>
+          <p className="text-2xl font-poppins mt-1">I am {""}
+            <span>
+              {text}
+            </span>
+            <Cursor />
+          </p> 
+          <button className="text-xl font-poppins text-center p-2 bg-yellow text-dark-blue rounded-full mt-3">
+            Contact Me
+          </button>
+          
+        </motion.div>
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,22 @@ export default {
         grey: "#EDEDED",
         "deep-blue": "#010026",
         "dark-grey": "#757575",
-        "opaque-black": "rgba(0,0,0,0.35"
+        "opaque-black": "rgba(0,0,0,0.35)",
+        "neon-purple": "#FF00FF",
       },
       backgroundImage: (theme) => ({
         "gradient-rainbow":
           "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
           "gradient-rainblue":
           "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+          "gradient-custom-1":
+          'linear-gradient(80deg,  #160729 10%, #050816 60%)',
       }),
       fontFamily: {
         playfair: ["Playfair Display", "serif"],
         poppins: ["Poppins", "sans-serif"],
         opensans: ["Open Sans", "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
       },
       content: {
         brush: "url('/src/assets/brush.png')",
